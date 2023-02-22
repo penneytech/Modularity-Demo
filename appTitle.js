390994b46f6d564029e38a557e55417af3a7968f
Paul Penney <>
2023-02-22 14:17:20 +0000

    Initial commit

---
+++ appTitle.js
@@ -0,0 +1,10 @@
+export function myTitle() {
+    const mytitle = "This is my title";
+    return mytitle;
+}
+
+export function mySecondTitle() {
+    const mysecondtitle = "this is my second title";
+    return mysecondtitle;
+}
+
---
+++ script.js
@@ -0,0 +1,9 @@
+import { myTitle, mySecondTitle } from './appTitle.js'
+
+let title = document.getElementById('title');
+title.innerHTML = myTitle();
+
+let title2 = document.getElementById('title2');
+title2.innerHTML = mySecondTitle();
+
+
